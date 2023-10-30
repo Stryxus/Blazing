9b24f27bab60024c947bb912ec96f3736ae040a1
Connor Shearer <11709044+Stryxus@users.noreply.github.com>
2023-10-30 22:03:49 +0000

    Get almost everything working except TS ESLint

---
+++ Blazing.Webpack/app.ts
@@ -0,0 +1,163 @@
+import path from 'path';
+import process from 'process';
+
+import webpack, { Stats } from 'webpack';
+import * as HtmlWebpackPlugin from 'html-webpack-plugin';
+import * as MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import * as ESLintPlugin from 'eslint-webpack-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
+
+import BlazingMediaMinificationPlugin from './plugins/blazingMediaMinificationPlugin';
+import BlazingCachePlugin from './plugins/blazingCachePlugin';
+
+var argEnv: string | undefined;
+var argPath: string | undefined;
+
+process.argv.forEach((val, index, array) => {
+    if (val === '--env') {
+        argEnv = array[index + 1];
+    }
+    if (val === '--path') {
+        argPath = array[index + 1];
+    }
+});
+
+if (argEnv && argPath)
+{
+    const config: webpack.Configuration = {
+        mode: argEnv == 'dev' ? 'development' : 'production',
+        entry: path.resolve(argPath, 'wwwroot-dev', 'blazing-assets.js'),
+        context: path.resolve(argPath, 'wwwroot-dev'),
+        output: {
+            path: path.resolve(argPath, 'wwwroot'),
+            publicPath: '',
+            filename: '[name].[contenthash].js',
+            globalObject: 'this',
+            clean: true,
+            asyncChunks: true,
+            environment: {
+                arrowFunction: true,
+                bigIntLiteral: true,
+                const: true,
+                destructuring: true,
+                dynamicImport: true,
+                forOf: true,
+                module: true,
+                optionalChaining: true,
+                templateLiteral: true,
+            },
+            assetModuleFilename: (pathData: any) => {
+                let filepath = path.dirname(pathData.filename).split('/').slice(1).join('/');
+                filepath = filepath.includes('/') ? filepath.slice(12) : filepath.slice(11);
+                return `${filepath}/[name].[contenthash][ext]`;
+            },
+        },
+        stats: {
+            children: argEnv === 'dev',
+            errorDetails: argEnv === 'dev',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.sass$/i,
+                    use: [
+                        { loader: MiniCssExtractPlugin.loader },
+                        { loader: require.resolve('ts-loader'), options: { sourceMap: argEnv === 'dev' } },
+                        {
+                            loader: require.resolve('postcss-loader'),
+                            options: {
+                                postcssOptions: {
+                                    plugins: () => {
+                                        return [
+                                            require('autoprefixer')
+                                        ];
+                                    }
+                                }
+                            }
+                        },
+                        { loader: require.resolve('sass-loader'), options: { sourceMap: argEnv === 'dev' } },
+                    ],
+                },
+                {
+                    test: /\.ts$/i,
+                    loader: require.resolve('ts-loader'),
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.(png|json|mp4|aac|svg)$/i,
+                    type: 'asset',
+                },
+                {
+                    test: /\.woff2?$/,
+                    type: 'asset',
+                    generator: {
+                        filename: './fonts/[name].[contenthash][ext]',
+                    },
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin.default({
+                template: path.resolve(argPath, 'wwwroot-dev', 'index.html'),
+            }),
+            new MiniCssExtractPlugin.default({
+                filename: './[name].[contenthash].css',
+            }),
+            new ESLintPlugin.default(),
+            {
+                apply: (compiler: webpack.Compiler) => {
+                    if (argEnv === 'prod') {
+                        compiler.hooks.done.tap('DonePlugin', () => {
+                            setTimeout(() => {
+                                process.exit(0)
+                            })
+                        });
+                    }
+                }
+            },
+            new BlazingMediaMinificationPlugin(),
+            new BlazingCachePlugin(),
+        ],
+        optimization: {
+            splitChunks: {
+                maxSize: 100000,
+            },
+            minimize: true,
+            minimizer: [
+                '...',
+                new TerserPlugin({
+                    parallel: true,
+                }),
+            ],
+        },
+        performance: {
+            hints: argEnv === 'prod' ? 'warning' : false
+        },
+        resolve: {
+            extensions: ['.ts']
+        },
+    };
+
+    const watch: webpack.Watching = webpack(config).watch({ ignored: ['^.*\/(?!(node_modules))[^\/]+$'] }, (err: Error | null | undefined, stats: Stats | undefined) =>
+    {
+        if (err) console.error(err.stack || err);
+        if (stats)
+        {
+            const info = stats.toJson({
+                colors: true,
+            });
+            if (stats.hasErrors()) console.error(info.errors);
+            if (stats.hasWarnings()) console.warn(info.warnings);
+        }
+    });
+
+    if (argEnv === 'prod')
+    {
+        watch.close((err: any, stats: any) =>
+        {
+            if (err) console.error(err);
+        });
+    }
+} else {
+    console.error('Either or both of --env and --path were not declared!');
+}
---
+++ Blazing.Webpack/plugins/blazingCachePlugin.ts
@@ -0,0 +1,23 @@
+import webpack from 'webpack';
+
+export default class BlazingCachePlugin {
+    apply(compiler: webpack.Compiler) {
+        compiler.hooks.compilation.tap('BlazingCachePlugin', compilation => {
+            compilation.hooks.processAssets.tap({
+                name: 'BlazingCachePlugin',
+                stage: webpack.Compilation.PROCESS_ASSETS_STAGE_ADDITIONAL,
+            }, assets => {
+                const sources = compilation.compiler.webpack.sources;
+
+                var a: string[] = [];
+                Object.entries(assets).forEach(([pathname, source]) => {
+                    if (!pathname.endsWith('.html') && !pathname.endsWith('.js') && !pathname.endsWith('.css') && !pathname.endsWith('.woff') && !pathname.endsWith('.woff2')) {
+                        a.push(pathname.startsWith('/') ? pathname.substring(1) : pathname);
+                    }
+                });
+                compilation.deleteAsset('bac.json');
+                compilation.emitAsset('bac.json', new sources.RawSource(JSON.stringify({a})));
+            });
+        });
+    }
+}
