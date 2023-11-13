8b3b3473a63df65cb70cba96a4cd7911518e126d
Connor Shearer <11709044+Stryxus@users.noreply.github.com>
2023-11-13 00:54:45 +0000

    Get the image processor working

--- Blazing.Webpack/plugins/blazingMediaMinificationPlugin.ts
+++ Blazing.Webpack/plugins/blazingMediaMinificationPlugin.ts
@@ -1,7 +1,7 @@
 import crypto from 'crypto';
 
-const sharp = require('sharp');
-const sizeOf = require('image-size');
+import sharp, { OutputInfo } from 'sharp';
+import sizeOf from 'image-size';
 
 const intl = new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 });
 const maxWidthHeight = 3840;
@@ -11,71 +11,87 @@ var caches: string[] = [];
 import webpack from 'webpack';
 
 export default class BlazingMediaMinificationPlugin {
-    apply(compiler: webpack.Compiler) {
-        compiler.hooks.compilation.tap('BlazingMediaMinificationPlugin', compilation => {
-            compilation.hooks.processAssets.tap({
+    apply(compiler: webpack.Compiler)
+    {
+        compiler.hooks.compilation.tap('BlazingMediaMinificationPlugin', compilation =>
+        {
+            compilation.hooks.processAssets.tapPromise({
                 name: 'BlazingMediaMinificationPlugin',
-                stage: webpack.Compilation.PROCESS_ASSETS_STAGE_ADDITIONAL,
-            }, assets => {
+                stage: webpack.Compilation.PROCESS_ASSETS_STAGE_ADDITIONS,
+            }, async assets => {
                 const sources = compilation.compiler.webpack.sources;
 
-                Object.entries(assets).forEach(([pathname, source]) => {
-                    //const assetInfo = compilation.assetsInfo.get(pathname);
-
-                    if (pathname.endsWith('.png') && pathname.startsWith('img/')) {
+                for (const [pathname, source] of Object.entries(assets))
+                {
+                    const assetInfo = compilation.assetsInfo.get(pathname);
+                    if (assetInfo && assetInfo.sourceFilename && pathname.endsWith('.png') && assetInfo.sourceFilename.startsWith('img/'))
+                    {
                         const imgSize = sizeOf(source.buffer());
 
-                        const hash = crypto.createHash('sha1');
-                        hash.setEncoding('hex');
-                        hash.write(source.buffer());
-                        hash.end();
+                        if (imgSize && imgSize.width && imgSize.height)
+                        {
 
-                        const sha1 = hash.read();
-                        if (caches.length >= 100000) caches = [];
-                        if (!caches.includes(sha1)) {
-                            caches.push(sha1);
+                            const hash = crypto.createHash('sha1');
+                            hash.setEncoding('hex');
+                            hash.write(source.buffer());
+                            hash.end();
 
-                            var avif: Buffer | undefined;
-                            var webp: Buffer | undefined;
-                            const img = sharp(source.buffer());
-                            const ratio = Math.min(maxWidthHeight / imgSize.width, maxWidthHeight / imgSize.height);
-                            var qualityDecrement = 0;
-                            for (var x = 1; x > 0; x -= 0.25) {
-                                let max = Math.floor(maxWidthHeight * x);
+                            const sha1 = hash.read();
+                            if (caches.length >= 100000) caches = [];
+                            if (!caches.includes(sha1))
+                            {
+                                caches.push(sha1);
 
-                                if (x < 1) {
-                                    if (imgSize.height > imgSize.width) {
-                                        if (imgSize.width > max) img.resize(max, Math.floor(imgSize.height * ratio));
-                                        if (imgSize.height > max) img.resize(Math.floor(imgSize.width * ratio), max);
+                                var avif: Buffer | undefined;
+                                var webp: Buffer | undefined;
+                                const img = sharp(source.buffer());
+                                const ratio = Math.min(maxWidthHeight / imgSize.width, maxWidthHeight / imgSize.height);
+                                var qualityDecrement = 0;
+                                for (var x = 1; x > 0; x -= 0.25)
+                                {
+                                    let max = Math.floor(maxWidthHeight * x);
+
+                                    if (x < 1)
+                                    {
+                                        if (imgSize.height > imgSize.width)
+                                        {
+                                            if (imgSize.width > max) img.resize(max, Math.floor(imgSize.height * ratio));
+                                            if (imgSize.height > max) img.resize(Math.floor(imgSize.width * ratio), max);
+                                        }
+                                        else
+                                        {
+                                            if (imgSize.height > max) img.resize(Math.floor(imgSize.width * ratio), max);
+                                            if (imgSize.width > max) img.resize(max, Math.floor(imgSize.height * ratio));
+                                        }
                                     }
-                                    else {
-                                        if (imgSize.height > max) img.resize(Math.floor(imgSize.width * ratio), max);
-                                        if (imgSize.width > max) img.resize(max, Math.floor(imgSize.height * ratio));
-                                    }
-                                }
 
-                                for (var y = 0; y < 15; y++) {
-                                    qualityDecrement = y;
-                                    img.avif({ quality: 100 - (y * 5), effort: process.env.NODE_ENV === 'production' ? 6 : 0 })
-                                    img.toBuffer({ resolveWithObject: true }, (err: any, buffer: any, info: any) => avif = buffer);
+                                    for (var y = 0; y < 15; y++)
+                                    {
+                                        qualityDecrement = y;
+                                        img.avif({ quality: 100 - (y * 5), effort: process.env.NODE_ENV === 'production' ? 6 : 0 })
+                                        avif = await img.toBuffer();
+                                        if (avif && avif.byteLength <= byteMaxSize) break;
+                                    }
                                     if (avif && avif.byteLength <= byteMaxSize) break;
                                 }
-                                if (avif && avif.byteLength <= byteMaxSize) break;
-                            }
 
-                            img.webp({ quality: 100 - (qualityDecrement * 5), effort: process.env.NODE_ENV === 'production' ? 6 : 0 })
-                            img.toBuffer({ resolveWithObject: true }, (err: any, buffer: any, info: any) => webp = buffer);
+                                img.webp({ quality: 100 - (qualityDecrement * 5), effort: process.env.NODE_ENV === 'production' ? 6 : 0 })
+                                webp = await img.toBuffer();
 
-                            // TODO: Need to find a way to get around Sharp being exclusively async since it cannot be used here.
-
-                            if (avif && webp) { // This should never fail but it makes the analyser happy.
-                                compilation.deleteAsset(pathname);
-                                compilation.emitAsset(pathname.replace('.png', '.avif'), new sources.RawSource(avif));
-                                compilation.emitAsset(pathname.replace('.png', '.webp'), new sources.RawSource(webp));
+                                if (avif && webp)
+                                { // This should never fail but it makes the analyser happy.
+                                    compilation.deleteAsset(pathname);
+                                    compilation.emitAsset(pathname.replace('.png', '.avif'), new sources.RawSource(avif));
+                                    compilation.emitAsset(pathname.replace('.png', '.webp'), new sources.RawSource(webp));
+                                }
                             }
                         }
+                        else
+                        {
+                            console.error('For some reason, the image\'s width and height could not be retreived!');
+                        }
                     }
-                });
+                }
             });
         });
     }
