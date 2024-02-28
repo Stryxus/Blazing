4a8c3fb85ac0eb5c3c179ad37e0e8ce4657d0595
Stryxus <11709044+Stryxus@users.noreply.github.com>
2024-02-28 15:22:51 +0000

    Move some code over from my website

---
+++ Blazing/Browser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.JSInterop;
+
+namespace Blazing;
+
+public class Browser
+{
+    public class LocalStorage
+    {
+        public async Task SetItem(IJSRuntime IJS, string key, string value) => await IJS.InvokeVoidAsync("runtime.localstorage.set", key, value);
+
+        public async Task<string> GetItem(IJSRuntime IJS, string key) => await IJS.InvokeAsync<string>("runtime.localstorage.get", key);
+    }
+}
---
+++ Blazing/Utilities/Services.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+public static class Services
+{
+    public static IConfiguration? Configuration { get; private set; }
+    public static IServiceProvider? Provider { get; private set; }
+    public static void SetConfiguration(IConfiguration configuration) => Configuration ??= configuration;
+    public static void SetServiceProvider(IServiceProvider provider) => Provider ??= provider;
+    public static void Get<T>(out T? service) => service = (T?)Provider?.GetService(typeof(T));
+    public static T? Get<T>() => (T?)Provider?.GetService(typeof(T));
+}
